7082b8241315273c50f79db04b01fa070e1c0bd6
MarkuS Sussitz <markus.sussitz@gmail.com>
2017-08-05 15:35:08 +0200

    Changed shebang for better portability.

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ./node_modules/.bin/sequelize db:migrate
 ./node_modules/.bin/nodemon --inspect=0.0.0.0 ./bin/www
--- start-prod.sh
+++ start-prod.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ./node_modules/.bin/sequelize db:migrate
 npm start

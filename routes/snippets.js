dd391ca6da1372a8aef0ebae72b0415f2bb2a614
MarkuS Sussitz <markus.sussitz@gmail.com>
2017-08-07 01:37:29 +0200

    Crud of snippet works. Update is tricky but works.

--- routes/snippets.js
+++ routes/snippets.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sequelize = require('sequelize');
+const _ = require('lodash');
 
 const db = require('../models');
 
@@ -6,7 +8,9 @@ const router = express.Router();
 
 router.get('/:id', async(req, res, next) => {
     try {
-        var snippet = await db.snippet.findById(req.params.id);
+        var snippet = await db.snippet.findById(req.params.id, {
+            include: ['tags', 'files']
+        });
 
         if (!snippet) {
             return res.sendStatus(404);
@@ -23,7 +27,8 @@ router.get('/', async(req, res, next) => {
         var snippets = await db.snippet.findAll({
             where: {
                 creatorId: req.user.sub
-            }
+            },
+            include: ['tags']
         });
 
         res.json(snippets);
@@ -46,5 +51,134 @@ router.post('/', async(req, res, next) => {
     }
 });
 
+router.put('/:id', async(req, res, next) => {
+    try {
+        var snippet = await db.snippet.findById(req.params.id, {
+            include: ['files', 'tags']
+        });
+
+        if (!snippet) {
+            return res.sendStatus(404);
+        }
+
+        var transaction = await db.sequelize.transaction();
+
+        try {
+            await updateFiles(snippet.id, snippet.files, req.body.files, transaction);
+            await updateTags(snippet.id, snippet.tags, req.body.tags, transaction);
+
+            await snippet.update(req.body, {
+                transaction: transaction
+            });
+
+            await transaction.commit();
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
+        }
+
+        res.json(snippet);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete('/:id', async(req, res, next) => {
+    try {
+        var snippet = await db.snippet.findById(req.params.id);
+
+        if (!snippet) {
+            return res.sendStatus(404);
+        }
+
+        await snippet.destroy();
+
+        res.sendStatus(200);
+    } catch (err) {
+        next(err);
+    }
+});
+
+async function updateFiles(snippetId, oldFiles, newFiles, transaction) {
+    var filesToAdd = _.differenceBy(newFiles, oldFiles, 'id').map(file => {
+        return {
+            name: file.name,
+            content: file.content,
+            order: file.order,
+            snippetId: snippetId
+        }
+    });
+
+    var filesToDelete = _.differenceBy(oldFiles, newFiles, 'id').map(file => file.id);
+    var filesToUpdate = _.intersectionBy(oldFiles, newFiles, 'id');
+
+    var createPromise = db.file.bulkCreate(filesToAdd, {
+        transaction: transaction
+    });
+
+    var destroyPromise = db.file.destroy({
+        where: {
+            id: {
+                $in: filesToDelete
+            }
+        }
+    }, {
+        transaction: transaction
+    });
+
+    var updateFilePromises = filesToUpdate.map(file => {
+        var updateContent = _.find(newFiles, {
+            id: file.id
+        });
+
+        return file.update(updateContent, {
+            transaction: transaction
+        });
+    });
+
+    var updatePromise = Promise.all(updateFilePromises);
+
+    await Promise.all([createPromise, destroyPromise, updatePromise]);
+}
+
+async function updateTags(snippetId, oldTags, newTags, transaction) {
+    var tagsToAdd = _.differenceBy(newTags, oldTags, 'id').map(tag => {
+        return {
+            text: tag.text,
+            snippetId: snippetId
+        }
+    });
+
+    var tagsToDelete = _.differenceBy(oldTags, newTags, 'id').map(tag => tag.id);
+    var tagsToUpdate = _.intersectionBy(oldTags, newTags, 'id');
+
+    var createPromise = db.tag.bulkCreate(tagsToAdd, {
+        transaction: transaction
+    });
+
+    var destroyPromise = db.tag.destroy({
+        where: {
+            id: {
+                $in: tagsToDelete
+            }
+        }
+    }, {
+        transaction: transaction
+    });
+
+    var updateTagPromises = tagsToUpdate.map(tag => {
+        var updateContent = _.find(newTags, {
+            id: tag.id
+        });
+
+        return tag.update(updateContent, {
+            transaction: transaction
+        });
+    });
+
+    var updatePromise = Promise.all(updateTagPromises);
+
+    await Promise.all([createPromise, destroyPromise, updatePromise]);
+}
 
 module.exports = router;
